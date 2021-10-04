2b549b998cb426cf7a89f3f0040c19e211de8b89
Blanc family <tuyanablanc@gmail.com>
2021-10-03 21:19:46 -0400

    updated Second class again

--- src/setup/Second.java
+++ src/setup/Second.java
@@ -2,12 +2,15 @@ package setup;
 
 public class Second {
     public void m(){
+        System.out.println("first");
 
     }
     public void m(int a){
+        System.out.println("second");
 
     }
     public void m(String s){
+        System.out.println("third");
 
     }
 }
